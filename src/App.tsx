6f1ccd62cca4400c3b5ad8695b8d8ef68119b6ef
Роман Мухаметшин <roman.muhametshin@triafly.ru>
2025-04-10 13:28:23 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { RoadMapPage } from './pages/RoadMapPage';

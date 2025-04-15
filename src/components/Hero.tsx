345d971aeea4fdb675d1c745e3825232b1e77d82
Oleg <mitinom98@gmail.com>
2025-04-15 14:23:16 +0300

    fix-text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
-import { ArrowRight } from 'lucide-react';
-import { AnimatedBackground } from './AnimatedBackground';
-import { useScrollTo } from '../hooks/useScrollTo';
-import { Link } from 'react-router-dom';
+import { ArrowRight } from "lucide-react";
+import { AnimatedBackground } from "./AnimatedBackground";
+import { useScrollTo } from "../hooks/useScrollTo";
+import { Link } from "react-router-dom";
 
 export function Hero() {
   const scrollTo = useScrollTo();
@@ -14,27 +14,31 @@ export function Hero() {
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-gray-900 dark:text-white mb-6">
             React-разработка с нуля:
             <br />
-            <span className="text-purple-600 dark:text-purple-400">ваш путь в Frontend!</span>
+            <span className="text-purple-600 dark:text-purple-400">
+              ваш путь в Frontend!
+            </span>
           </h1>
           <p className="text-xl text-gray-600 dark:text-gray-300 mb-8 max-w-3xl mx-auto">
-            Подробный курс? Нет не просто курс, мы сообщество, которое погрузит вас в мир web - разработки. Изучите основы
-            программирования, создайте реальные проекты и подготовьтесь к карьере разработчика.
+            Подробный курс? Нет мы не просто курс, мы сообщество, которое
+            погрузит вас в мир web - разработки. Изучите основы
+            программирования, создайте реальные проекты и подготовьтесь к
+            карьере разработчика.
           </p>
           <div className="flex justify-center space-x-4">
-            <button 
-              onClick={() => scrollTo('contact-form')}
+            <button
+              onClick={() => scrollTo("contact-form")}
               className="bg-purple-600 text-white px-8 py-3 rounded-lg hover:bg-purple-700 transition-colors flex items-center space-x-2"
             >
               <span>Начать обучение</span>
               <ArrowRight className="h-5 w-5" />
             </button>
 
-						<Link 
-            to="/smart-start"
+            <Link
+              to="/smart-start"
               className="border-2 border-purple-600 text-purple-600 dark:text-purple-400 dark:border-purple-400 px-8 py-3 rounded-lg hover:bg-purple-50 dark:hover:bg-purple-900/50 transition-colors"
-          >
-            Узнать больше
-          </Link>
+            >
+              Узнать больше
+            </Link>
           </div>
         </div>
       </div>
